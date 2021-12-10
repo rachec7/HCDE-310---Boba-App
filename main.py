ef45835eb434d451d6e8358df7d42babb7e7e6f8
Jocelyn Chen <jocelynchen1126@yahoo.com>
2021-12-09 19:53:34 -0800

    edits from class

--- main.py
+++ main.py
@@ -9,73 +9,73 @@ import logging
 app = Flask(__name__)
 
 def get_one_day_emotion(body = ""):
-  headers = {
+    headers = {
     "User-Agent": "Rachel's Food Menu App",
     "apikey": "UdBdTBiWyMZRa741w31wW9xmAvFEjLfe"
-  }
-  baseurl = "https://api.promptapi.com/text_to_emotion"
-  data = urllib.parse.quote(body).encode("utf-8")
-  req = urllib.request.Request(baseurl, headers = headers, data = data)
-  resp = urllib.request.urlopen(req)
-  urlread = resp.read()
-  jsonurl = json.loads(urlread)
-  return jsonurl
+    }
+    baseurl = "https://api.promptapi.com/text_to_emotion"
+    data = urllib.parse.quote(body).encode("utf-8")
+    req = urllib.request.Request(baseurl, headers = headers, data = data)
+    resp = urllib.request.urlopen(req)
+    urlread = resp.read()
+    jsonurl = json.loads(urlread)
+    return jsonurl
 
 def get_one_day_emotion_safe(body = ""):
-  try:
-    return get_one_day_emotion(body)
-  except urllib.error.URLError as e:
-    print("Error trying to retrieve data:", e)
+    try:
+        return get_one_day_emotion(body)
+    except urllib.error.URLError as e:
+        print("Error trying to retrieve data:", e)
     return None
 
 def compare_day_emotions(body1,body2):
-  params1 = {}
-  params2 = {}
+    params1 = {}
+    params2 = {}
 
-  params1['body'] = body1
-  params2['body'] = body2
+    params1['body'] = body1
+    params2['body'] = body2
 
-  dict1 = get_one_day_emotion_safe(body = body1)
-  dict2 = get_one_day_emotion_safe(body = body2)
+    dict1 = get_one_day_emotion_safe(body = body1)
+    dict2 = get_one_day_emotion_safe(body = body2)
 
-  happy1 = dict1['Happy']
-  angry1 = dict1['Angry']
-  surprise1 = dict1['Surprise']
-  sad1 = dict1['Sad']
-  fear1 = dict1['Fear']
+    happy1 = dict1['Happy']
+    angry1 = dict1['Angry']
+    surprise1 = dict1['Surprise']
+    sad1 = dict1['Sad']
+    fear1 = dict1['Fear']
 
-  happy2 = dict2['Happy']
-  angry2 = dict2['Angry']
-  surprise2 = dict2['Surprise']
-  sad2 = dict2['Sad']
-  fear2 = dict2['Fear']
+    happy2 = dict2['Happy']
+    angry2 = dict2['Angry']
+    surprise2 = dict2['Surprise']
+    sad2 = dict2['Sad']
+    fear2 = dict2['Fear']
 
-  final_happy = (happy1 + happy2)/2
-  final_angry = (angry1 + angry2)/2
-  final_surprise = (surprise1 + surprise2)/2
-  final_sad = (sad1 + sad2)/2
-  final_fear = (fear1 + fear2)/2
+    final_happy = (happy1 + happy2)/2
+    final_angry = (angry1 + angry2)/2
+    final_surprise = (surprise1 + surprise2)/2
+    final_sad = (sad1 + sad2)/2
+    final_fear = (fear1 + fear2)/2
 
-  final_emotions_dict = {}
-  final_emotions_dict['Happy'] = final_happy
-  final_emotions_dict['Angry'] = final_angry
-  final_emotions_dict['Surprise'] = final_surprise
-  final_emotions_dict['Sad'] = final_sad
-  final_emotions_dict['Fear'] = final_fear
+    final_emotions_dict = {}
+    final_emotions_dict['Happy'] = final_happy
+    final_emotions_dict['Angry'] = final_angry
+    final_emotions_dict['Surprise'] = final_surprise
+    final_emotions_dict['Sad'] = final_sad
+    final_emotions_dict['Fear'] = final_fear
 
-  return final_emotions_dict
+    return final_emotions_dict
 
 def compare_day_emotions_safe(body1,body2):
-  try:
-    return compare_day_emotions(body1,body2)
-  except urllib.error.URLError as e:
-    print("Error trying to retrieve data:", e)
+    try:
+        return compare_day_emotions(body1,body2)
+    except urllib.error.URLError as e:
+        print("Error trying to retrieve data:", e)
     return None
 
 """sorts emotions from highest to lowest"""
 def sortKeysByValue(dict):
-  keys = dict.keys()
-  return sorted(keys, key=lambda k: dict[k], reverse=True)
+    keys = dict.keys()
+    return sorted(keys, key=lambda k: dict[k], reverse=True)
 
 
 
@@ -339,15 +339,6 @@ def input_form():
             prompt = "How can we give you a drink suggestion if you don't fill everything out? Please fill out the form :)")    
     else:
         return render_template('input-template.html', page_title = "Input Form")
-
-
-
-
-
-
-
-
-
 
 
 
