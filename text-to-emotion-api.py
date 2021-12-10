1079e415e4e92e28286e0871271e11369ec86fbb
rachec7 <91706317+rachec7@users.noreply.github.com>
2021-12-09 16:36:01 -0800

    Add files via upload

--- text-to-emotion-api.py
+++ text-to-emotion-api.py
@@ -1,15 +1,8 @@
 import urllib.parse, urllib.request, urllib.error, json
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
-@app.route("/")
-def input_form():
-  return render_template('application.html')
-
-if __name__ == "__main__":
-  app.run(host="localhost", port=8080, debug=True)
 
 def get_one_day_emotion(body = ""):
   headers = {
@@ -17,8 +10,11 @@ def get_one_day_emotion(body = ""):
     "apikey": "UdBdTBiWyMZRa741w31wW9xmAvFEjLfe"
   }
   baseurl = "https://api.promptapi.com/text_to_emotion"
+  print(body)
   data = urllib.parse.quote(body).encode("utf-8")
+  print(data)
   req = urllib.request.Request(baseurl, headers = headers, data = data)
+  print(req)
   resp = urllib.request.urlopen(req)
   urlread = resp.read()
   jsonurl = json.loads(urlread)
@@ -40,6 +36,8 @@ def compare_day_emotions(body1,body2):
 
   dict1 = get_one_day_emotion_safe(body = body1)
   dict2 = get_one_day_emotion_safe(body = body2)
+  print(dict1)
+  print(dict2)
 
   happy1 = dict1['Happy']
   angry1 = dict1['Angry']
@@ -75,16 +73,33 @@ def compare_day_emotions_safe(body1,body2):
     print("Error trying to retrieve data:", e)
     return None
 
-#print(compare_day_emotions_safe(input1,input2))
-
-final_emotions_dict = compare_day_emotions_safe(input1,input2)
+#final_emotions_dict = compare_day_emotions_safe(input_1,input_2)
 
 """sorts emotions from highest to lowest"""
 def sortKeysByValue(dict):
   keys = dict.keys()
   return sorted(keys, key=lambda k: dict[k], reverse=True)
 
-"""returns strongest emotion as a string"""
-emotionstring = ""
-emotionstring = sortKeysByValue(final_emotions_dict)[0]
-print(emotionstring)
+@app.route("/", methods = ["GET","POST"])
+def input_form():
+  input_1 = request.form.get('{input_1}')
+  input_2 = request.form.get('{input_2}')
+  app.logger.info(input_1)
+  app.logger.info(input_2)
+  if request.method == "POST":
+    if input_1 and input_2:
+    # if form filled in, return string of strongest emotion
+      final_emotions_dict_safe = compare_day_emotions_safe(input_1,input_2)
+      print(final_emotions_dict_safe)
+      emotionstring = sortKeysByValue(final_emotions_dict_safe)[0]
+      #return render_template('emotionstring.html',page_title="Success",emotionstring = emotionstring)
+    else:
+    # if not, then show the form again with a correction to the user
+      return render_template('application.html',
+                           page_title="Boba Form - Error",
+                           prompt="How can I generate a boba drink for you if you don't enter how you are feeling on both days?")
+  else:
+    return render_template('application.html', page_title = "Input Form")
+
+if __name__ == "__main__":
+  app.run(host="localhost", port=8080, debug=True)
